b99e6c867f586eebc2eaf47078f7e7bd5b94d719
shaneliebenthal <shaneliebenthal@gmail.com>
2025-01-19 01:21:19 -0500

    Get Deploy to Vercel working using vercel postgres

--- src/app/server/db/index.ts
+++ src/app/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import "../../../../envConfig.ts";
-import * as schema from "./schema.ts";
+import * as schema from "./schema";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.POSTGRES_URL!,
 });
 
 export const db = drizzle(pool, { schema });
