9db4848c54790ac3a34c2f17f0068348041d1987
shaneliebenthal <shaneliebenthal@gmail.com>
2025-04-20 21:16:29 -0400

    Major Refactore to Tanstack Start, start with clean install of tst and get initial working version of plain start

--- src/app/admin/workflow/[id]/forms/searchParams.tsx
+++
@@ -1,8 +0,0 @@
-import { createLoader, parseAsString } from 'nuqs/server'
- 
-// Describe your search params, and reuse this in useQueryStates / createSerializer:
-export const workflowAdminPageSearchParams = {
-  state: parseAsString.withDefault(''),
-}
- 
-export const loadSearchParams = createLoader(workflowAdminPageSearchParams)
