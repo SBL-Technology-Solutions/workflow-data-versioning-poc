81c5dd2b064e0923b52ffb609ea464fb3b70a056
shaneliebenthal <shaneliebenthal@gmail.com>
2025-04-20 23:16:33 -0400

    Add Biome

--- app/ssr.tsx
+++ app/ssr.tsx
@@ -1,13 +1,13 @@
 // app/ssr.tsx
 import { getRouterManifest } from "@tanstack/react-start/router-manifest";
 import {
-  createStartHandler,
-  defaultStreamHandler,
+	createStartHandler,
+	defaultStreamHandler,
 } from "@tanstack/react-start/server";
 
 import { createRouter } from "./router";
 
 export default createStartHandler({
-  createRouter,
-  getRouterManifest,
+	createRouter,
+	getRouterManifest,
 })(defaultStreamHandler);
