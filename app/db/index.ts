ddaace098675a6095ab030692bc300eb6c08ff4f
shaneliebenthal <shaneliebenthal@gmail.com>
2025-04-25 22:43:13 -0400

    add t3-env package to zod validate env variables

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,12 +1,7 @@
+import { env } from "@/env";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const dbUrl = process.env.DATABASE_URL as string;
-
-if (!dbUrl) {
-	console.error("DATABASE_URL is not set");
-}
-
-const sql = neon(dbUrl);
+const sql = neon(env.DATABASE_URL);
 export const db = drizzle({ client: sql, schema });
