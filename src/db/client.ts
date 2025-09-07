417fb15cc9197161d324f3c98a4403c0fe7c4593
shaneliebenthal <shaneliebenthal@gmail.com>
2025-09-07 02:24:30 -0400

    Refactor DbTransaction type definition and remove unused db.ts types file

--- src/db/client.ts
+++ src/db/client.ts
@@ -8,3 +8,7 @@ const pool = new Pool({
 });
 
 export const dbClient = drizzle(pool, { schema, casing: "snake_case" });
+
+export type DbTransaction = Parameters<
+	Parameters<typeof dbClient.transaction>[0]
+>[0];
--- src/types/db.ts
+++
@@ -1,6 +0,0 @@
-import { dbClient } from "@/db/client";
-
-export type DbTransaction = Parameters<
-	Parameters<typeof dbClient.transaction>[0]
->[0];
-export type DbClient = typeof dbClient;
